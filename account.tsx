cf368657062a963bf702938f85a71969e676cab6
sooy <ssy67366736@gmail.com>
2024-06-28 17:34:24 +0900

    수정중

---
+++ account.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi';
+
+export const Account = () => {
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { data: ensName } = useEnsName({ address });
+  const { data: ensAvatar } = useEnsAvatar({
+    name: ensName as string | undefined,
+  });
+
+  return (
+    <div>
+      {ensAvatar && <img alt='ENS Avatar' src={ensAvatar} />}
+      {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
+      <button onClick={() => disconnect()}>Disconnect</button>
+    </div>
+  );
+};
