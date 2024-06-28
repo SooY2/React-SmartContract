97f519d9c8f0115350d466774cabf8e1c7c3a6ce
sooy <ssy67366736@gmail.com>
2024-06-28 13:43:45 +0900

    잠시 중단

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import { WagmiProvider } from 'wagmi';
+import { config } from '../config';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
+    </WagmiProvider>
+  );
 }
 
 export default App;
